cddb6d3a4586824c09a5ff4c62c770fb55544f7d
Simon Wilson <wsn123654@gmail.com>
2018-03-30 17:45:13 +0100

    Initial Commit
    Initial Commit

---
+++ News/News/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace News
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            /*routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Accounts", action = "Login", id = UrlParameter.Optional }
+            );*/
+
+            routes.MapRoute(
+              "Default",
+              "{controller}/{action}/{id}",
+              new { controller = "Accounts", action = "Login", id = UrlParameter.Optional },
+              new[] { "News.Controllers" }
+            );
+
+        }
+    }
+}
